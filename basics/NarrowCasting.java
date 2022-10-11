8ce2ac8e81d853be3c3f0a44540e35543cd80425
NomulaDileepReddy <dileepreddynomula@gmail.com>
2022-10-11 23:38:18 +0530

    first commit

---
+++ basics/NarrowCasting.java
@@ -0,0 +1,8 @@
+public class NarrowCasting{
+    public static void main(String args[])
+    {
+        float number1=7.8f;
+        int intNumber=(int)number1;
+        System.out.println(intNumber);
+    }
+}
---
+++ basics/javalesson.java
@@ -0,0 +1,8 @@
+public class javalesson{
+    public static void main(String args[])
+    {
+        int variable1=10;
+        int variable2=20;
+        System.out.println(variable1+variable2);
+    }
+}
